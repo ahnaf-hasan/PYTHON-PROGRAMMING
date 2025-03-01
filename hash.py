6e63453176fa8bfbe3c77436491513d319c50ba2
ahnaf-hasan <ahnaf.cse8741@gmail.com>
2025-03-01 10:26:07 +0600

    add new file

---
+++ hash.py
@@ -0,0 +1,13 @@
+# Let's Start
+def print_star_grid(rows, cols):
+    for row in range(rows):
+        for col in range(cols):
+            print('#', end=' ')
+        print()
+
+# Define these
+rows = 3
+cols = 5
+
+# print it
+print_star_grid(rows, cols)
